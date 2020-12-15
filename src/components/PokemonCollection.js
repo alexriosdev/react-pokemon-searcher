e398d1ae6e6d3704fed2a3c8640e8b2067d491c6
Alex Rios <alexrios.dev@gmail.com>
2020-12-15 15:30:54 -0600

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,7 +2,8 @@ import React from 'react'
 import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
-class PokemonCollection extends React.Component {  render() {
+class PokemonCollection extends React.Component {
+  render() {
     const result = this.props.pokemon.map(p => <PokemonCard key={p.id} pokemon={p} />)
     return (      
       <Card.Group itemsPerRow={6}>
